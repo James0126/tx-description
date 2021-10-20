523b43d5d36b9aaefa4f1557dcc6786cbcc4135a
James <james@terra.money>
2021-10-20 12:13:53 +0900

    clean up codes

--- src/helpers/utility.ts
+++ src/helpers/utility.ts
@@ -1,6 +1,3 @@
-import axios from "axios";
-
-export const ASSETS = axios.create({ baseURL: "https://assets.terra.money" });
 export const REGEXP = {
   ADDRESS: /(terra1[a-z0-9]{38})|(terravaloper[a-z0-9]{39})/g,
   COIN: /^\d+((terra1[a-z0-9]{38})|(u[a-z]{1,4}))/g,
